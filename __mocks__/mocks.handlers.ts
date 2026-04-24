370b027e145464d46f70fda0c7acae4bbc785280
NamesCooper <cooper.flick@outlook.com>
2026-04-24 17:05:26 +1000

    fresh nuxt project

--- __mocks__/mocks.handlers.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler } from "h3";
-import mockDataHandler from "./handlers/data.mock";
-
-export default defineEventHandler((event) => {
-	switch (event.node.req.url?.split("?")[0]) {
-		case "/api/data":
-			return mockDataHandler(event);
-	}
-});
